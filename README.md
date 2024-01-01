af467065a4f460c86ccc932342daee5119c84e95
Rafael Neves de Oliveira <rafael.noliveira@totvs.com.br>
2024-01-01 16:45:43 -0300

    sidebar

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# CSS Modules
+
+- Módulos CSS não interagem com os elementos fora do componente o qual estão alocados. Seu arquivo é criado seguindo o exemplo: Header.module.css e sua estrutura é feita de classes e não id's. No inspect elements ao inspecionar a classe css feita utilizando a técnica de CSS Módulos é notado que ao invez do nome da classe aparece uma Hash (hexadecimal), ou seja, aquela classe pertence apenas a aquela tag ...entenda como algo amarrado.
+
+Notas:
+
+- box-sizing: border-box --> quando a tela é reduzida, não expande ou vaza ele espreme o conteudo interno
+
+1rem = 16px
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
-import "./styles.css"
+import { Sidebar } from "./components/Sidebar";
+import styles from './App.module.css'
+
+import "./global.css"
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post
-        author="rafael"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto recusandae deleniti aliquam neque! Voluptatem laborum amet quasi, aut error neque saepe eaque nobis dolorum odit delectus ipsam incidunt deleniti? Ut?"
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="rafael"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto recusandae deleniti aliquam neque! Voluptatem laborum amet quasi, aut error neque saepe eaque nobis dolorum odit delectus ipsam incidunt deleniti? Ut?"
+          />
+        </main>
+      </div>
     </div>
   );
 }
